a84e96cdc380ce96e1a6a08fc5b46de7bce4bb13
Takowski <93830775+Takowski@users.noreply.github.com>
2024-02-23 22:55:25 +0100

    3.2

---
+++ index.js
@@ -0,0 +1,43 @@
+const express= require('express')
+const app = express()
+
+let persons =[
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons',(req,res)=>{
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const Personscount = persons.length
+    const  date = new Date().toString()
+    res.send(`
+        <p>Phonebook has info for ${Personscount} people</p>
+        <p>${date}</p>
+    `)
+  })
+
+const Port =3001
+app.listen(Port,()=>{
+    console.log(`Server running on port ${Port}`)
+})
